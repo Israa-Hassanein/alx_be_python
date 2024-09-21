4bc1b76637619a38101e9ee5f155925c37b03f7d
israa-hassanein <israa.hassanein@gmail.com>
2024-09-21 23:47:01 +0300

    update weather advice file

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,9 @@
-#weather advice task 0
-current_weather = str(input("What's the weather like today? (sunny/rainy/cold): ")).lower()
-if current_weather == "sunny":
+weather = str(input("What's the weather like today? (sunny/rainy/cold): ")).lower()
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif current_weather == "sunny":
+elif weather == "sunny":
     print("Don't forget your umbrella and a raincoat.")
-elif current_weather ==  "cold":
+elif weather ==  "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
