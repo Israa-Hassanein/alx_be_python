f3777d22343f07f991dabd581638b189b81616c0
israa-hassanein <israa.hassanein@gmail.com>
2024-09-29 02:24:33 +0300

    arithmetic operation changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,22 +1,15 @@
 def perform_operation(num1, num2, operation):
-    # Dictionary to map operation strings to corresponding functions
-    operations = {
-        "add": lambda x, y: x + y,
-        "subtract": lambda x, y: x - y,
-        "multiply": lambda x, y: x * y,
-        "divide": lambda x, y: x / y if y != 0 else "Cannot divide by zero"
-    }
-    
-    # Check if the operation is valid
-    if operation not in operations:
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "multiply":
+        result = num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Cannot divide by zero"
+        result = num1 / num2
+    else:
         return "Not a valid operation"
-    
-    try:
-        # Perform the requested operation
-        result = operations[operation](num1, num2)
-    except ZeroDivisionError:
-        result = "Cannot divide by zero"
-    except Exception as e:
-        result = f"An error occurred: {str(e)}"
     
     return result
